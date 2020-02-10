027ca581449290b0b6fa532d637cb37ca22a5794
jonnylagos115 <lagosjonathan1@gmail.com>
2020-02-10 13:14:48 -0800

    Add files via upload

--- 22B_H_4/22B_H4B_Ptr_Sort.cpp
+++ 22B_H_4/22B_H4B_Ptr_Sort.cpp
@@ -1,50 +1,50 @@
 /**
    CIS 22B: Homework 4B
-   C-strings and arrays of pointers 
-   
+   C-strings and arrays of pointers
+
    This program uses a selection sort to sort an array of c-strings.
    It has some errors. Fix the errors and run the program.
    Save the output as a comment at the end of the program.
- 
+
    NAME:
 */
 
 /**
  A NOTE about pointers and constants: pointer constant versus pointer to constant
- 
- 
+
+
  const char *list[10] = {"Mary", "John", "Peter", "Anne",
                          "Paul",  "Andrew", "Linda",  "Diane"};
- 
+
  What is list?
- 
+
  list is an array of 10 pointers to string literals (or constants):
  the strings in the array may not be changed but the pointers pointing to these strings could change.
- 
+
  Therefore the following statement is incorrect
  strcpy(list[0], "Tom"); // This is an attempt to change a constant: replace "Mary" by "Tom"
- 
+
  It is OK to change the pointers in the list:
  list[0] = list[1]; // now both pointers point to "John"
- 
+
  Here are some examples
- 
+
  EXAMPLE ONE: A pointer variable pointing to a constant
  const double PI = 3.14;
  const double EPSILON = 0.001;
  const double *ptr = &PI;
  cout << *ptr << endl;
- 
+
  // *ptr += 1;     //<==== this doesn't work because PI should not change
  ptr = &EPSILON;  // but you are allowed to change ptr, to make it point to another constant
  cout << *ptr << endl;
- 
+
  EXAMPLE TWO: A pointer constant pointing to a constant
  const double PI = 3.14;
  const double EPSILON = 0.001;
  const double * const ptr = &PI; // both ptr and its pointee (PI) are constants
  cout << *ptr << endl;
- 
+
  // *ptr += 10; // doesn't work because its pointee is PI, a constant (it may not be changed)
  // ptr = &EPSILON;  // doesn't work because because ptr is also a constant (it may not be changed)
 
@@ -80,7 +80,7 @@ int main( void )
    	Given: ary  - a list of strings
 	       size   - its actual size
 */
-void selectionSort( const char *list[],  int size )
+void selectionSort( const char *list[],  int size ) //Make corrections,
 {
     const char *hold;
     int last;
@@ -93,7 +93,7 @@ void selectionSort( const char *list[],  int size )
     {
         small = curr;
 		for( walk = curr + 1; walk < size; walk++ )
-		    if( list[walk] < list[small] )
+		    if( list[walk] < list[small] ) //use strcmp()
 			    small = walk;
 
         hold = list[small];
@@ -104,8 +104,8 @@ void selectionSort( const char *list[],  int size )
 }
 /***************************************************************
  Save the OUTPUT below
- 
- 
+
+
  */
 
 
--- 22B_H_4/22B_H4C_Performers.cpp
+++ 22B_H_4/22B_H4C_Performers.cpp
@@ -1,12 +1,12 @@
 /**
  CIS 22B: Homework 4C
  Strings and Structures
- 
+
  Project: Star Search
- 
- NAME:
- IDE:
- 
+
+ NAME: Jonathan Lagos
+ IDE: None (Linux, with VSCode
+
  */
 
 #include <iostream>
@@ -15,14 +15,14 @@ using namespace std;
 
 
 /* Define a struct named Performer with the following fields:
- 
+
  1. name (string)
  2. scores (an array of 5 doubles)
  3. final (double)
- 
+
  */
 
-// Function prototypes
+// Function prototypes, feel free to change the data return type
 void printWelcome(void);
 void readPerfData();    // <== add parameters, change void if needed
 void calculateScore();  // <== add parameters, change void if needed
@@ -35,7 +35,7 @@ int main( void )
 {
     string inFileName[] = {"performers.txt", "contestants.txt", "test.txt", "testempty.txt", ""};
     int choice = 1;  // to stop the program to allow the user to see the results one file at a time
-    
+
     for (string *ptr = inFileName; choice == 1 && *ptr != ""; ptr++)    // test loop: takes the names of 4 input files from an array
     {
         printWelcome();
@@ -45,16 +45,16 @@ int main( void )
         writeFile();        // <== change call to match with definition
         displayResults();   // <== change call to match with definition
         printEnd();
-        
-        
+
+
         cout << "Please enter 1 to continue 0 to stop" << endl;
         cin >> choice;
     }
-    
+
     return 0;
 }   // main
 /**********************
- 
+
  */
 void printWelcome(void)
 {
@@ -62,7 +62,7 @@ void printWelcome(void)
 }
 
 /**********************
- 
+
  */
 void readPerfData()    // <== add parameters, change void if needed
 {
@@ -70,7 +70,7 @@ void readPerfData()    // <== add parameters, change void if needed
 }
 
 /**********************
- 
+
  */
 void calculateScore()  // <== add parameters, change void if needed
 {
@@ -78,7 +78,7 @@ void calculateScore()  // <== add parameters, change void if needed
 }
 
 /**********************
- 
+
  */
 void insertionSort()   // <== add parameters, change void if needed
 {
@@ -86,7 +86,7 @@ void insertionSort()   // <== add parameters, change void if needed
 }
 
 /**********************
- 
+
  */
 void writeFile()       // <== add parameters, change void if needed
 {
@@ -94,7 +94,7 @@ void writeFile()       // <== add parameters, change void if needed
 }
 
 /**********************
- 
+
  */
 void displayResults()  // <== add parameters, change void if needed
 {
@@ -102,7 +102,7 @@ void displayResults()  // <== add parameters, change void if needed
 }
 
 /**********************
- 
+
  */
 void printEnd(void)
 {
@@ -110,6 +110,6 @@ void printEnd(void)
 }
 
 /** Save the output below
- 
- 
+
+
  */
